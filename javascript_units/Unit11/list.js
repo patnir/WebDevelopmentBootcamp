88c12d0f74f9988e044369e054b2b960d181f130
patnir <rahul20patni@gmail.com>
2017-03-14 18:52:24 -0400

    finishing up js

--- javascript_units/Unit11/list.js
+++ javascript_units/Unit11/list.js
@@ -13,6 +13,20 @@ while (request != "quit") {
 	else if (request == "quit") {
 		alert("Bye!");
 	}
+	else if (request == "delete") {
+		if (todos.length == 0) {
+			alert("nothing to delete");
+		} else {
+			todo = prompt("what do you want to delete?");
+			index = todos.indexOf(todo);
+			if (index != -1) {
+				todos.splice(index, 1);
+			}
+			else {
+				alert("does not exist!");
+			}
+		}
+	}
 	else {
 		alert("Invalid command!");
 	}
