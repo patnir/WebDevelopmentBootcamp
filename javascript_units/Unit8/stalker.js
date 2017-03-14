52dd557b605dddbebb94eedaa30b1c2bfcb27f2b
patnir <rahul20patni@gmail.com>
2017-03-14 15:03:59 -0400

    Javascript unit 8

---
+++ javascript_units/Unit8/stalker.js
@@ -0,0 +1,5 @@
+var firstname = prompt("What is you first name?");
+var lastname = prompt("What is your last name?");
+var age = prompt("WHat is you age?");
+alert("Your name is " + firstname + " " + lastname + ".");
+alert("You are " + age + " years old.");
