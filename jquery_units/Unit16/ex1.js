1e785d7180d135f14d375259dac3a40241bdda1c
patnir <rahul20patni@gmail.com>
2017-03-16 19:19:35 -0400

    excersice and notes jquery

---
+++ jquery_units/Unit16/ex1.js
@@ -0,0 +1,6 @@
+$("div").css("background-color", "purple");
+$("div.highlight").css("width", "200px");
+$("#third").css("border", "orange solid 1px");
+$("div:nth-of-type(1)").css("color", "pink");
+// div:first-of-type
+// div:first - slower than first of type
