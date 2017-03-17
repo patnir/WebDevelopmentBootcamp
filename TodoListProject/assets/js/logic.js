ad20a9c4b6fdaa016fefa1cd4cfaf8cd20594fda
patnir <rahul20patni@gmail.com>
2017-03-16 23:34:28 -0400

    clicking on X to remove todo

--- TodoListProject/assets/js/logic.js
+++ TodoListProject/assets/js/logic.js
@@ -19,3 +19,15 @@ $("li").click(function() {
 	}*/
 	$(this).toggleClass("taskCompleted");
 });
+
+// click on X to delete task
+$(".deleteTask").click(function(event) {
+	console.log("asdf");
+	event.stopPropagation();
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+});
+
+// Where it says: $("input[type='text'").keypress(function(event){ 
+// it should actually be: $("input[type='text']").keypress(function(event){ the 
