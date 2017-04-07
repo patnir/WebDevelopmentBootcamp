d7c0a207eb9787464ef9a9d11e9882ee18d9ec54
patnir <rahul20patni@gmail.com>
2017-04-07 08:01:14 -0400

    couple of requests

--- express_units/pickingUp/FirstExpressApp/app.js
+++ express_units/pickingUp/FirstExpressApp/app.js
@@ -1,1 +1,23 @@
 console.log("asdf this is cool");
+
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res) => {
+	res.send("print baby!");
+});
+
+app.get("/bye", (req, res) => {
+	res.send("print babyeby!");
+});
+
+
+app.get("*", (req, res) => {
+	res.send("dude, wrong page")
+}); // order of routes matters, 
+// if this were at the top then 
+// other routes would be accessed
+
+app.listen(3000, () => {
+	console.log("listening on PORT 3000");
+});
