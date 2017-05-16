267f0b1c227a7f5f49a7550c65a2a2f24d84084e
patnir <rahul20patni@gmail.com>
2017-05-15 21:37:24 -0700

    working with apis and json

---
+++ apis/testing1/firstRequest.js
@@ -0,0 +1,12 @@
+var request = require("request");
+request("https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", function(error, response, body) {
+	if (error) {
+		console.log("something went wrong");
+		console.log(error);
+	} else {
+		if (response.statusCode == 200) {
+			data = JSON.parse(body);
+			console.log(data["query"]["results"]["channel"]["astronomy"]["sunset"]);
+		}
+	}
+});
