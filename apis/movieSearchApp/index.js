0a001551c09bcf62c64f62643fef640ebdb33147
patnir <rahul20patni@gmail.com>
2017-05-17 21:14:17 -0700

    updating index to add search page

--- apis/movieSearchApp/index.js
+++ apis/movieSearchApp/index.js
@@ -1,17 +1,26 @@
 var express = require("express");
 var app = express();
-
+app.set("view engine", "ejs");
 var request = require("request");
 
 app.listen(3000, () => {
 	console.log("Movie app has started");
 });
 
+app.get("/", (req, res)=> {
+	res.render("search");
+});
+
 app.get("/results", function(req, res) {
-	request("http://omdbapi.com/?s=california", function(error, response, body) {
+	//console.log(req.query.search);
+	var search = req.query.search;
+	var url = "http://omdbapi.com/?s=" + search;
+	request(url, function(error, response, body) {
 		if(!error && response.statusCode == 200) {
-			var results = JSON.parse(body);
-			res.send(results["Search"][0]);
+			var data = JSON.parse(body);
+			//res.send(results["Search"][0]);
+			res.render("results", {data: data});
+
 		}
 	})
 });
